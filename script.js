a3b21f326cc37e52a5f59e6a1b99f51766e4e00c
Akash_Paraskar <156954600+paraskaraakash@users.noreply.github.com>
2025-04-07 14:54:17 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -7,87 +7,78 @@ let level = 0;
 
 let h2 = document.querySelector("h2");
 
-document.addEventListener("keypress", function () {
-    if (started == false) {
-        console.log("game is started");
-        started = true;
-
-        levelup();
-    }
+document.querySelector("#start-btn").addEventListener("click", () => {
+  if (!started) {
+    started = true;
+    levelup();
+  }
 });
 
 function gameflash(btn) {
-    btn.classList.add("flash");
-    setTimeout(function () {
-        btn.classList.remove("flash");
-    }, 100);
+  btn.classList.add("flash");
+  setTimeout(() => {
+    btn.classList.remove("flash");
+  }, 300);
 }
-
 
 function userflash(btn) {
-    btn.classList.add("userflash");
-    setTimeout(function () {
-        btn.classList.remove("userflash");
-    }, 100);
+  btn.classList.add("userflash");
+  setTimeout(() => {
+    btn.classList.remove("userflash");
+  }, 300);
 }
-function levelup() {
-    userseq = [];
-    level++;
-    h2.innerText = `level ${level}`;
 
-    //random btn chooose
-    let randomIdx = Math.floor(Math.random() * 3);
-    let rancolor = btns[randomIdx];
-    let randbtn = document.querySelector(`.${rancolor}`);
-    // console.log(rancolor);
-    // console.log(randbtn)
-    // console.log(randomIdx)
-    gameseq.push(rancolor);
-    console.log(gameseq)
-    gameflash(randbtn);
+function levelup() {
+  userseq = [];
+  level++;
+  h2.innerText = `Level ${level}`;
+  let randomIdx = Math.floor(Math.random() * 4);
+  let color = btns[randomIdx];
+  let randbtn = document.querySelector(`.${color}`);
+  gameseq.push(color);
+  gameflash(randbtn);
 }
 
 function checkAns(idx) {
-
-    if (userseq[idx] === gameseq[idx]) {
-        if (userseq.length == gameseq.length) {
-            setTimeout(levelup, 1000);
-        }
+  if (userseq[idx] === gameseq[idx]) {
+    if (userseq.length === gameseq.length) {
+      setTimeout(levelup, 1000);
     }
-    else {
-        h2.innerHTML = `Game over! your Score Was <b>${level}</br> press any key to start.`;
-
-        document.querySelector("body").style.backgroundColor = "red";
-        setTimeout(function () {
-            document.querySelector("body").style.backgroundColor = "white";
-        }, 100);
-
-        reset();
-    }
+  } else {
+    h2.innerHTML = `Game Over! Your Score: <b>${level}</b><br>Click "Start Game" to try again.`;
+    document.body.style.backgroundColor = "red";
+    setTimeout(() => {
+      document.body.style.backgroundColor = "";
+    }, 300);
+    reset();
+  }
 }
-
-
 
 function btnpress() {
-
-    let btn = this;
-    userflash(btn);
-
-    usercolor = btn.getAttribute("id");
-    userseq.push(usercolor);
-
-    checkAns(userseq.length - 1);
+  let btn = this;
+  let color = btn.getAttribute("id");
+  userseq.push(color);
+  userflash(btn);
+  checkAns(userseq.length - 1);
 }
-
 
 let allbtns = document.querySelectorAll(".btn");
-for (btn of allbtns) {
-    btn.addEventListener("click", btnpress);
-}
+allbtns.forEach(btn => {
+  btn.addEventListener("click", btnpress);
+});
 
 function reset() {
-    started = false;
-    gameseq = [];
-    userseq = [];
-    level = 0;
+  gameseq = [];
+  userseq = [];
+  started = false;
+  level = 0;
+}
+
+// Footer page open (you can later create these .html pages)
+function openTerms() {
+  window.location.href = "terms.html";
+}
+
+function openHelp() {
+  window.location.href = "contact.html";
 }
